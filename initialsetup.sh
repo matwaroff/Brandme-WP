74bc8a97ad0b08f2c82269bb3f9e79f03a1eeb3c
Mat Waroff <matwaroff@gmail.com>
2016-12-11 19:23:03 -0500

    Imporvements

--- initialsetup.sh
+++ initialsetup.sh
@@ -6,11 +6,14 @@ else
 	sudo adduser mat
 	name="mat"
 fi
+sudo -i
 sudo echo "$name     ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
+logout
 sudo ssh-keygen
 sudo -u$name ssh-keygen
 sudo vi ~/.bashrc
 sudo apt-get install apache2 mysql-server php vsftpd
 sudo mysql_secure_installation
+sudo echo '[mysqld]\nsql-mode=' >> /etc/mysql/my.cnf
 sudo vi /etc/apache2/sites-available/000-default.conf
 sudo service apache2 restart
